6f6c736c3059aed77cc541df519b9898b1f5f532
Rclodeca <ryan@theclodes.com>
2018-06-21 17:08:39 -0700

    added image link form, rank, and dynamic background

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import './App.css';
 import Navigation from './components/navigation/Navigation.js';
 import Logo from './components/logo/Logo.js';
 import 'tachyons';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm.js';
+import Rank from './components/rank/Rank.js';
+import Particles from 'react-particles-js';
+
+
+const particleOptions = {
+   particles: {
+      number: {
+         value: 60,
+         density: {
+            enable: true,
+            value_area: 400
+         }
+      }
+   },
+   interactivity: {
+      detect_on: "window",
+      events: {
+         onhover: { enable: true, mode: "repulse" },
+         onclick: { enable: true, mode: "push" },
+         resize: true
+      }
+   }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+            className='particles'
+            params={particleOptions}
+         />
         <Navigation />
-        <Logo />
-        {/*<ImageLinkForm />
-        <FaceRecognition />*/}
+        <Logo />  
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+const Rank = () => {
+	return (
+		<div className=''> 
+			<div className='white f3'>
+				{'Ryan, your current rank is '}
+			</div>
+			<div className='white f1'>
+				{'#1'}
+			</div>
+		</div>
+		);
+}
+
+export default Rank;
