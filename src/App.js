88daf82247bea94d99154191cb32bf0b6728e2d8
Rclodeca <ryan@theclodes.com>
2018-06-21 19:02:11 -0700

    added face recognition via URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import 'tachyons';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm.js';
 import Rank from './components/rank/Rank.js';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import { key } from './Api.json';
+import FaceRecognition from './components/faceRecognition/FaceRecognition.js';
 
+
+const app = new Clarifai.App({
+ apiKey: key
+});
 
 const particleOptions = {
    particles: {
@@ -28,21 +35,54 @@ const particleOptions = {
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Particles 
+
+   constructor() {
+      super();
+      this.state = {
+         input: '',
+         imageURL: ''
+      }
+   }
+
+   onInputChange = (event) => {
+      this.setState({ input: event.target.value});
+   }
+
+   onButtonSubmit = () => {
+      this.setState({ imageURL: this.state.input})
+      app.models.predict(
+         Clarifai.FACE_DETECT_MODEL, 
+         this.state.input
+      )
+      .then(
+         function(response) {
+            console.log(response);
+            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+         },
+         function(err) {
+            console.log(err);
+         }
+      );
+   }
+
+   render() {
+      return (
+         <div className="App">
+         <Particles 
             className='particles'
             params={particleOptions}
          />
-        <Navigation />
-        <Logo />  
-        <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+         <Navigation />
+         <Logo />  
+         <Rank />
+         <ImageLinkForm   
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+         />
+         <FaceRecognition imageURL={this.state.imageURL} />
       </div>
-    );
-  }
+      );
+   }
 }
 
 export default App;
